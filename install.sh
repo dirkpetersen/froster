a68cdd24affc6294fdb2fb3453abdad95a1954b6
Dirk Petersen <peterdir@ohsu.edu>
2023-04-29 08:32:16 -0700

    froster

--- install.sh
+++ install.sh
@@ -6,26 +6,31 @@ echo ""
 echo "Installing Froster, please wait ..."
 P3=$(which python3)
 if [[ -z ${P3} ]]; then
-  echo "python3 could not be found, please install first"
+  echo "python3 could not be found, please install Python >= 3.${PMIN} first"
   exit
 fi
 if [[ $(${P3} -c "import sys; print(sys.version_info >= (3,${PMIN}))") == "False" ]]; then
   echo "Python >= 3.${PMIN} required and your default ${P3} is too old."
   if [[ -n ${LMOD_ROOT} ]]; then
-    echo "LMOD detected, trying to load Python..."
+    printf "Lmod detected, trying to load Python ... "
     ml python > /dev/null 2>&1
     ml Python/3.8 > /dev/null 2>&1
+    echo "Done!"
     if [[ $(python3 -c "import sys; print(sys.version_info >= (3,${PMIN}))") == "False" ]]; then
       echo "Python >= 3.${PMIN} required but the active python3 is too old."
       exit
-    fi 
-    printf "Done!"
+    fi
+  elif [[ -n ${SPACK_ROOT} ]]; then
+    printf "Spack detected, you can run 'spack load python'"
   else
     echo "please load a diffent Python >= 3.${PMIN} and try again"
+    if [[ -n ${SPACK_ROOT} ]]; then
+      printf "Spack detected, you can also run 'spack load python'"
+    fi
     exit
   fi
 fi
-echo "Install virtual environment ~/.local/share/froster ... "
+printf "Install virtual environment ~/.local/share/froster ... "
 rm -rf ~/.local/share/froster.bak
 mv ~/.local/share/froster ~/.local/share/froster.bak
 mkdir -p ~/.local/share/froster
@@ -33,12 +38,13 @@ mkdir -p ~/.local/bin
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 python3 -m venv ~/.local/share/froster
 source ~/.local/share/froster/bin/activate
-printf "Done!"
-echo "Installing packages ... "
+echo "Done!"
+printf "Installing packages ... "
+python3 -m pip install --upgrade pip
 curl -Ls https://raw.githubusercontent.com/dirkpetersen/froster/main/requirements.txt \
         -o ~/.local/share/froster/requirements.txt \
       && python3 -m pip install --upgrade -r ~/.local/share/froster/requirements.txt
-printf "Done!"
+Echo "Done!"
 
 curl -Ls https://raw.githubusercontent.com/dirkpetersen/froster/main/froster.py \
         -o ~/.local/bin/froster.py
