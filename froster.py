eea761c33f8458b593e52076ecc49f8a05e10441
Dirk Petersen <peterdir@ohsu.edu>
2023-10-25 00:37:03 -0700

    v 0.9.0.1

--- froster.py
+++ froster.py
@@ -21,7 +21,7 @@ from textual.widgets import Label, Input, LoadingIndicator
 from textual.widgets import DataTable, Footer, Button 
 
 __app__ = 'Froster, a user friendly S3/Glacier archiving tool'
-__version__ = '0.9.0'
+__version__ = '0.9.0.1'
 
 def main():
         
