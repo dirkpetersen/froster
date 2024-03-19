938d6c149bc99c5b4cd232b1f08cc9bc0981cbb2
Victor Machado <victor.machado@hpcnow.com>
2024-03-19 10:58:04 +0100

    s3-restore.sh formatted

--- s3-restore.sh
+++ s3-restore.sh
@@ -1,27 +1,27 @@
 #! /bin/bash
 
 # A simple script for restoring data that has been archived with Froster.
-# Use this script if you find that Froster is not working or no longer 
+# Use this script if you find that Froster is not working or no longer
 # maintained. The only dependencies of this script are rclone and jq.
 # This script should work until the end of times !
-# 
+#
 # Report issues here: https://github.com/dirkpetersen/froster/issues
 
-export RCLONE_S3_PROFILE=aws # or ${AWS_PROFILE} or change this to the AWS profile you want to use
+export RCLONE_S3_PROFILE=aws      # or ${AWS_PROFILE} or change this to the AWS profile you want to use
 export RCLONE_S3_REGION=us-west-2 # or change this to the AWS region you want to use
-export RCLONE_S3_PROVIDER=AWS # or change this to another S3 provider (e.g. Ceph for on-premises)
-export RCLONE_S3_ENV_AUTH=true # use AWS environment variables and settings from ~/.aws
+export RCLONE_S3_PROVIDER=AWS     # or change this to another S3 provider (e.g. Ceph for on-premises)
+export RCLONE_S3_ENV_AUTH=true    # use AWS environment variables and settings from ~/.aws
 #export RCLONE_S3_ENDPOINT='https://s3.us-west-2.amazonaws.com'
 TAR_FILENAME="Froster.smallfiles.tar"
 
 if [[ -z $(which rclone) ]]; then
-    echo "rclone not installed or not in PATH. Check here: https://rclone.org/downloads"
-    exit 1
+  echo "rclone not installed or not in PATH. Check here: https://rclone.org/downloads"
+  exit 1
 fi
 
 if [[ -z $(which jq) ]]; then
-    echo "jq not installed or not in PATH. Check here: https://jqlang.github.io/jq/download/"
-    exit 1
+  echo "jq not installed or not in PATH. Check here: https://jqlang.github.io/jq/download/"
+  exit 1
 fi
 
 PROFS=$(grep '^\[' ~/.aws/credentials | tr -d '[]')
@@ -31,10 +31,10 @@ if ! echo "${PROFS}" | grep -q "\<${RCLONE_S3_PROFILE}\>"; then
 fi
 echo "Using ~/.aws profile: ${RCLONE_S3_PROFILE}"
 
-# check if this is a shared config 
-CFG_ROOT=~/.config/froster
-if [[ -e ${CFG_ROOT}/config_root ]]; then
-  CFG_ROOT=$(cat ${CFG_ROOT}/config_root)
+# check if this is a shared config
+CFG_ROOT=~/.froster/config
+if [[ -e ${CFG_ROOT}/shared_config_dir ]]; then
+  CFG_ROOT=$(cat ${CFG_ROOT}/shared_config_dir)
   if [[ ! -d ${CFG_ROOT} ]]; then
     echo "Config root ${CFG_ROOT} not found"
     exit 1
@@ -43,10 +43,10 @@ fi
 
 FOLDERS=$(jq -r .[].local_folder ${CFG_ROOT}/froster-archives.json)
 
-if [[ $1 == "list" ]] || [[ -z $1 ]] ; then
+if [[ $1 == "list" ]] || [[ -z $1 ]]; then
   echo "Available local folders:"
   echo "$FOLDERS"
-  echo -e "\nUsage: $0 <local_folder>"  
+  echo -e "\nUsage: $0 <local_folder>"
   exit
 fi
 
@@ -97,7 +97,7 @@ fi
 ## If data is in Glacier, retrieve it first before restoring
 if [[ "${S3_CLASS}" == "GLACIER" ]] || [[ "${S3_CLASS}" == "DEEP_ARCHIVE" ]]; then
   echo "Checking Glacier retrieve (${ARCHIVE_MODE}) ... "
-  rclone backend restore ${DEPTH} -o priority=Bulk -o lifetime=30 ${ARCHIVE_FOLDER} > ~/.rclone-glacier-restore.json
+  rclone backend restore ${DEPTH} -o priority=Bulk -o lifetime=30 ${ARCHIVE_FOLDER} >~/.rclone-glacier-restore.json
   status_list=$(jq -r .[].Status ~/.rclone-glacier-restore.json)
   if echo "$status_list" | grep -q "RestoreAlreadyInProgress"; then
     echo "Retrieve: Glacier retrieve in progress, try 5-12 hours after restore was initiated."
@@ -105,17 +105,17 @@ if [[ "${S3_CLASS}" == "GLACIER" ]] || [[ "${S3_CLASS}" == "DEEP_ARCHIVE" ]]; th
   else
     echo "Retrieve: Glacier retrieve initiated."
     if [[ "${ARCHIVE_MODE}" == "Recursive" ]]; then
-      sleep 5 
+      sleep 5
     fi
   fi
   if [[ "${ARCHIVE_MODE}" == "Recursive" ]]; then # --max-depth does not seem to work with backend restore-status, fix later
-    rclone backend restore-status ${DEPTH} ${ARCHIVE_FOLDER} > ~/.rclone-glacier-restore.json
+    rclone backend restore-status ${DEPTH} ${ARCHIVE_FOLDER} >~/.rclone-glacier-restore.json
     status_list=$(jq -r .[].RestoreStatus.IsRestoreInProgress ~/.rclone-glacier-restore.json)
     if echo "$status_list" | grep -xq "true"; then
       echo "Retrieve Status: Glacier restore still in progress, try 5-12 hours after restore was initiated."
       exit 1
     fi
-  fi 
+  fi
 fi
 
 if [[ ! -d ${FLD} ]]; then
@@ -127,7 +127,7 @@ fi
 echo "Restoring from ${ARCHIVE_FOLDER} to ${FLD} ..."
 rclone copy --checksum --progress --verbose ${ARCHIVE_FOLDER} ${FLD} ${DEPTH}
 
-## Comparing S3 with local folder 
+## Comparing S3 with local folder
 echo "Running Checksum comparison, hit ctrl+c to cancel ... "
 rclone check --verbose --exclude='.froster.md5sum' ${ARCHIVE_FOLDER} ${FLD} ${DEPTH}
 
@@ -151,6 +151,6 @@ for tar_file in "${tar_files[@]}"; do
     echo "Failed to extract: $tar_file"
   fi
   # Change back to the original directory
-  cd - > /dev/null || return
+  cd - >/dev/null || return
 done
-cd "$cdir" 
+cd "$cdir"
