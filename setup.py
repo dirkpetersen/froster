a80ca9f165117b31f551cdc7401ef434bb1ee3b7
Victor Machado <victor.machado@hpcnow.com>
2024-05-14 15:49:05 +0200

    Increased version number to 0.10.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='froster',
-    version='0.10.4',
+    version='0.10.5',
     license='MIT',
     packages=find_packages(),
     install_requires=requirements,
