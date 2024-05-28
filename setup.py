89457e2f102557e96d0434f15d1546b566c8dbda
victor machado <victor.machado@hpcnow.com>
2024-05-28 09:52:14 +0000

    version to 0.10.14

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='froster',
-    version='0.10.13',
+    version='0.10.14',
     license='MIT',
     packages=find_packages(),
     install_requires=requirements,
