b5672e348be2541fc4d4699fae49ae30376d28c5
victor machado <victor.machado@hpcnow.com>
2024-05-30 10:35:00 +0000

    upd version to v0.10.22

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='froster',
-    version='0.10.21',
+    version='0.10.22',
     license='MIT',
     packages=find_packages(),
     install_requires=requirements,
